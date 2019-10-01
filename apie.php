df426a6c888dad6f5552de2d0eced8a75b75c6dd
N0NCore <N0NCore@users.noreply.github.com>
2019-10-01 21:34:45 +0300

    apie, style and script

--- apie.php
+++ apie.php
@@ -2,13 +2,15 @@
 <html>
 <head>
 	<title>About</title>
-<meta charset="utf-8">
+<!-- <meta charset="utf-8">
  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 <link rel="stylesheet" type="text/css" href="styles/style.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
- 
+  -->
+<?php include "header-script.php"; ?>
+
 </head>
 <body class="grey">
 
@@ -27,51 +29,86 @@
 
   
   <div class="container">
-<h1 class="gray lighten-4 white-text text-darken-4 center-align">APIE</h1>
-</div>
-
- <div class="container">
-   <style>
-.carousel .carousel-item {
-  width: 100%;
-}
-</style>
-
-<ul id="tabs-swipe-demo" class="tabs">
-  <li class="tab col s3"><a class="active" href="#swipe-1">Test 1</a></li>
-  <li class="tab col s3"><a href="#swipe-2">Test 2</a></li>
-  <li class="tab col s3"><a href="#swipe-3">Test 3</a></li>
-</ul>
-<div id="swipe-1" class="col s12 blue">First tab content</div>
-<div id="swipe-2" class="col s12 red">Second tab content</div>
-<div id="swipe-3" class="col s12 green">Third tab content</div>
-</div>
+<h1 class="gray lighten-4 white-text text-darken-4 center-align col l1 s1">APIE MUS</h1>
  
+<!-- Tab links -->
+<div class="tab container blue darken-1">
+  <button class="tablinks " onclick="openCity(event, 'Lora')">Lora</button>
+  <button class="tablinks " onclick="openCity(event, 'Raimis')">Raimis</button>
+  <button class="tablinks " onclick="openCity(event, 'Aliona')">Aliona-Jelena</button>
+   <button class="tablinks" onclick="openCity(event, 'Auris')">Auris</button>
+</div>
 
-<footer class= "page-footer grey darken-3">
-     <div class="container">
-   <div class="row">
-   <div class="col s12 l6">
-   <h5>About Me</h5>
-   <p>Patys kiečiausi laiptų gamintojai Lietuvoje</p>
-   <p>Patys kiečiausi laiptų gamintojai Lietuvoje</p>
-   </div>
-   <div class="col s12 l4 offset-l2">
-   <h5>Connect</h5>
-   <ul>
-   <li><a href="#" class="grey-text text-lighten-3">Facebook</a> </li>
-   <li><a href="#" class="grey-text text-lighten-3">LinkedIn</a> </li>
-   <li><a href="#" class="grey-text text-lighten-3">Patreon</a> </li>
-   </ul>
-   </div>
+<!-- Tab content -->
+<div id="Lora" class="tabcontent center-align deep-purple lighten-3">
+  <h3 style=>Lora</h3>
+  <img class="materialboxed responsive-img container center-align" src="https://picsum.photos/150">
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+  proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</p>
+</div>
+
+<div id="Raimis" class="tabcontent center-align deep-purple lighten-3">
+  <h3>Raimis</h3>
+  <img class="materialboxed responsive-img container center-align" src="https://picsum.photos/150">
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+  proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</p>
+</div>
+
+<div id="Aliona" class="tabcontent center-align deep-purple lighten-3">
+  <h3>Aliona-Jelena</h3>
+  <img class="materialboxed responsive-img container center-align" src="https://picsum.photos/150">
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+  proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</p>
+</div>
+
+<div id="Auris" class="tabcontent center-align deep-purple lighten-3">
+  <h3>Auris</h3>
+  <img class="materialboxed responsive-img container center-align" src="https://picsum.photos/150">
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+  proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</p>
+</div>
+</div>
+<br>
+<br>
+<br>
+  <div class="row container">
+    <H3>Contact Us</H3>
+      <div class="col s6 ">
+        <img class="map" src="images/map.png">
+
+
+      </div>
+      <div class="col s6">
+        <ul>
+          <li> </li>
+        </ul>
+      </div>
     </div>
-    
-    <div class="footer-copyright grey darken-4">
-    <div ="container center-align">&copy; 2019 N0Ncore</div>
-    </div>
-  </footer>
-<!-- <?php include "footer.php"; ?>
-<?php include "footer-script.php"; ?> -->
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<?php include "footer.php"; ?>
+<?php include "footer-script.php"; ?>
        
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 <script type="text/javascript" src="scripts/script.js" ></script>
--- scripts/script.js
+++ scripts/script.js
@@ -4,10 +4,20 @@ $(document).ready(function(){
      $('.parallax').parallax();
       // $('.sidenav').sidenav();
       $(".dropdown-trigger").dropdown();
-      $('ul.tabs').tabs({
-  swipeable : true,
-  responsiveThreshold : Infinity
-});
-
   });
 
+
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
