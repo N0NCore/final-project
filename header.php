7770fd86e6a6197126bba9bb8211bc952fb5fb7d
Lora Breskaja <lora@Loras-MacBook.local>
2019-10-02 21:05:18 +0300

    Update header.php

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
         <nav>
             <div class="nav-wrapper deep-pink darken-2">
                 <!-- Logo -->
-                <a href="#" class="brand-logo">Logo</a>
+                <a href="#" class="brand-logo"><i class="material-icons">pets</i></i></a>
 
                 <!-- Ícone para abrir no Mobile -->
                 <a href="#" data-target="mobile-navbar" class="sidenav-trigger">
