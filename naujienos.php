b7105f92e06a29de48fa02c9d299cf7435ed2c94
Lora Breskaja <lora@Loras-MacBook.local>
2019-10-02 20:17:38 +0300

    Update naujienos.php

--- naujienos.php
+++ naujienos.php
@@ -24,51 +24,54 @@
 
 <div id="animals" class="container newalerts">
 
-      <div id="pattern" class="naujienulinija row">
+      <div id="pattern" class="naujienulinija row hoverable">
       <div class="col s12 m6 l3">
         <img class="pirmanuotrauka" src="images/tigerloose.jpg">
       </div>
-        <div class="col s12 m6 l7"> 
+        <div class="col s12 m6 l6"> 
         <ul class="naujienublokas">
           <li><h5 id="newstitle" class="naujienos">Breaking: Tiger on the loose!</h5></li>
           <li><p id="naujienosdata" class="pirmastekstas">2019-10-01</p></li>
-          <li><p class="pirmastekstas">Meduolis bus skanus</p></li>
-          <li><p class="pirmastekstas">Meduolis bus skanus</p></li>
+          <li><p class="pirmastekstas">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></li>
+          <li><p class="pirmastekstas"><a href="https://google.com/search">link text</a></p></li>
         </ul>
         </div>
-        <div class="col s12 m6 l2">
-        <form action="https://google.com/search">
-        <input type="text" name="query">
-        <button>Read more > </button>
-        </form>  
+        <div class="col s12 m6 l3">
+        <a class="waves-effect waves-light btn-small buttonnews"><i class="material-icons right">keyboard_arrow_right</i>Read more</a>  
         </div>
       </div>
 
-      <div id="pattern" class="naujienulinija row">
+      <div id="pattern" class="naujienulinija row hoverable">
       <div class="col s12 m6 l3">
         <img class="pirmanuotrauka" src="images/bowlittle.png">
       </div>
-        <div class="col s12 m6 l7"> 
+        <div class="col s12 m6 l6"> 
         <ul class="naujienublokas">
           <li><h5 id="newstitle" class="naujienos">Zoo animals for adoption</h5></li>
           <li><p id="naujienosdata" class="pirmastekstas">2019-09-16</p></li>
-          <li><p class="pirmastekstas">Meduolis bus skanus</p></li>
-          <li><p class="pirmastekstas">Meduolis bus skanus</p></li>
+          <li><p class="pirmastekstas">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></li>
+          <li><p class="pirmastekstas"><a href="https://google.com/search">link text</a></p></li>
         </ul>
-        </div>  
+        </div> 
+        <div class="col s12 m6 l3">
+        <a class="waves-effect waves-light btn-small buttonnews"><i class="material-icons right">keyboard_arrow_right</i>Read more</a> 
+        </div> 
       </div>
 
-      <div id="pattern" class="naujienulinija row">
+      <div id="pattern" class="naujienulinija row hoverable">
       <div class="col s12 m6 l3">
         <img class="pirmanuotrauka" src="images/slothfam.jpg">
       </div>
-        <div class="col s12 m6 l7"> 
+        <div class="col s12 m6 l6"> 
         <ul class="naujienublokas">
           <li><h5 id="newstitle" class="naujienos">Welcome two sloths to the family</h5></li>
           <li><p id="naujienosdata" class="pirmastekstas">2019-08-28</p></li>
-          <li><p class="pirmastekstas">Meduolis bus skanus </p></li>
-          <li><p class="pirmastekstas">Meduolis bus skanus </p></li>
+          <li><p class="pirmastekstas">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></li>
+          <li><p class="pirmastekstas"><a href="https://google.com/search">link text</a></p></li>
         </ul>
+        </div>
+        <div class="col s12 m6 l3">
+        <a class="waves-effect waves-light btn-small buttonnews"><i class="material-icons right">keyboard_arrow_right</i>Read more</a> 
         </div>  
       </div>
 
