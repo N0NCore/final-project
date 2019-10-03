e71a1a5f8a07515c68e94c49c323b134297e0efa
N0NCore <N0NCore@users.noreply.github.com>
2019-10-03 18:39:27 +0300

    styles, footer2

---
+++ footer2.php
@@ -0,0 +1,18 @@
+<footer class="page-footer">
+<section id="lab_social_icon_footer">
+<!-- Include Font Awesome Stylesheet in Header -->
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
+<div >
+        <div class="text-center center">
+                <a href="https://www.facebook.com/bootsnipp" target="_blank"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
+              <a href="https://twitter.com/bootsnipp"target="_blank"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
+              <a href="https://www.youtube.com/watch?v=Z_FKqLqkyPY&list=PL_q0VkY4KS9uNLZWfg2XbEKVwnWHqlC87&index=3&t=0s"target="_blank"><i id="social-yt" class="fa fa-youtube-square fa-3x social"></i></a>
+              <a href="mailto:#"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
+          
+  </div>
+  <div class="center">   © 2019 Copyright. All Rights Reserved  </div>
+</div>
+</section>
+    </footer>
+  
