09d1f1b0a2d74532a3cf6787faf8b9614a594481
N0NCore <N0NCore@users.noreply.github.com>
2019-10-03 21:31:53 +0300

    updated

--- apie.php
+++ apie.php
@@ -40,20 +40,21 @@
 <div id="Lora" class="tabcontent center-align transparent">
   <div class="tabcontentcontainer">
     <h3>Lora</h3>
-  <img class="materialboxed responsive-img center-align picz" src="https://picsum.photos/300">
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-  proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</p>
+    <div class="video-container">
+        <iframe width="853" height="480" src="//www.youtube.com/embed/SYcD64qX56s" frameborder="0" allowfullscreen></iframe>
+      </div> 
+<!--   <img class="materialboxed responsive-img center-align picz" src="https://picsum.photos/300"> -->
+  <p>Coming soon...</p>
 </div>
 </div>
 
 <div id="Raimis" class="tabcontent center-align transparent">
   <div class="tabcontentcontainer">
   <h3 >Raimis</h3>
-  <img class="materialboxed responsive-img center-align picz" src="https://picsum.photos/300">
+  <!-- <img class="materialboxed responsive-img center-align picz" src="https://picsum.photos/300"> -->
+    <div class="video-container">
+        <iframe width="853" height="480" src="//www.youtube.com/embed/WpZYJqiWBNE" frameborder="0" allowfullscreen></iframe>
+      </div> 
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
   tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
   quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -66,51 +67,50 @@
 <div id="Aliona" class="tabcontent center-align transparent">
    <div class="tabcontentcontainer">
   <h3 >Aliona</h3>
-  <img class="materialboxed responsive-img center-align picz" src="https://picsum.photos/300">
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-  proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</p>
+ <!--  <img class="materialboxed responsive-img center-align picz" src="https://picsum.photos/300"> -->
+    <div class="video-container">
+        <iframe width="853" height="480" src="//www.youtube.com/embed/uwmeH6Rnj2E" frameborder="0" allowfullscreen></iframe>
+      </div> 
+
+  <p>Yes.</p>
 </div>
 </div>
 
 <div id="Auris" class="tabcontent center-align transparent">
   <div class="tabcontentcontainer">
   <h3 >Auris</h3>
-  <img class="materialboxed responsive-img center-align picz" src="https://picsum.photos/300">
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-  proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</p>
+  <!-- <img class="materialboxed responsive-img center-align picz" src="https://picsum.photos/300"> -->
+  <div class="video-container">
+        <iframe width="853" height="480" src="//www.youtube.com/embed/L58lDForIqM" frameborder="0" allowfullscreen></iframe>
+      </div> 
+  <p>Today is just a gift, that's why it's the present</p>
  </div>
  </div>
 
  <div id="Community" class="tabcontent center-align transparent">
   <div class="tabcontentcontainer">
-  <h3 >Bendruomenė</h3>
-  <img class="materialboxed responsive-img center-align picz" src="https://picsum.photos/300">
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-  proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</p>
+  <h3 >Community</h3>
+  <img class="materialboxed responsive-img center-align picz" src="https://i.pinimg.com/originals/7b/0a/f2/7b0af23449e4c9d7d676cd3a9920eb1b.jpg">
+  <p>In Warsaw, Poland, Solly visits his rabbi and complains, 'Life is unbearable. There are nine of us living in one room. What can I do?'
+    The rabbi answers, 'Take your goat into the room with you.'
+    Solly is incredulous, 'What?' but the rabbi insists, 'Do as I say and come back in a week.'
+    A week later Solly comes back looking even more distraught than before, 'We cannot stand it,' Solly explains to the rabbi, 'the goat is filthy.'The rabbi then tells Solly, 'Go home and let the goat out. Come back in a week.'
+  </p>
 </div>
 </div>
 
 <div id="ThankYou" class="tabcontent center-align transparent">
   <div class="tabcontentcontainer">
   <h3 >AČIŪ !!!</h3>
-  <img class="materialboxed responsive-img center-align picz" src="https://picsum.photos/300">
+  <!-- <img class="materialboxed responsive-img center-align picz" src="https://picsum.photos/300"> -->
+  <div class="video-container">
+        <iframe width="853" height="480" src="//www.youtube.com/embed/yBLdQ1a4-JI" frameborder="0" allowfullscreen></iframe>
+      </div> 
   <p>AČIŪ!!! <br>Ačiū Ritai, Ačiū Viktorijai, Ačiū Kazimirui!</p>
 </div>
 </div>
 
-<div class="container contact">
+<div class="container contact red lighten-2">
   <div class="row">
     <div  class="col l6 s12 ">
       <h5 class="gray-text Right">Contact us</h5>
@@ -137,8 +137,8 @@
   <form action="/action_page.php" class="form-container">
     <h1>Ask Us</h1>
 
-    <label for="msg"><b>Hello World</b></label>
-    <textarea placeholder="Hi, my name is Taj. How can I help You?" name="msg" required></textarea>
+    <label for="msg"><b>Hello My Friend</b></label>
+    <textarea placeholder="Hi, my name is Dovakin. How can I help You?" name="msg" required></textarea>
 
     <button type="submit" class="btn">Send</button>
     <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
